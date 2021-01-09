cd2893791358e20b0c23fa9a2887f884cd91e062
Seunghun Lee <waydi1@gmail.com>
2021-01-09 19:12:58 +0900

    Chapter 18 solutions

---
+++ ch18/68-167-two-sum-ii-input-array-is-sorted.go
@@ -0,0 +1,20 @@
+package ch18
+
+func twoSum(numbers []int, target int) []int {
+	left := 0
+	right := len(numbers) - 1
+
+	for left < right {
+		sum := numbers[left] + numbers[right]
+
+		if sum > target {
+			right--
+		} else if sum < target {
+			left++
+		} else {
+			return []int{left + 1, right + 1}
+		}
+	}
+
+	return []int{-1, -1}
+}
